4ebe7a37848e3162fd656c23c1a756008611dc52
ViktorMartynov92 <sky.martinov@gmail.com>
2025-09-01 20:27:43 +0900

    Fixed exception

--- src/main/java/com/javarush/martynov/Main.java
+++ src/main/java/com/javarush/martynov/Main.java
@@ -2,6 +2,7 @@ package com.javarush.martynov;
 
 public class Main {
     public static void main(String[] args) {
+        //can be static
         CaesarCipherApp caesarCipherApp = new CaesarCipherApp();
         caesarCipherApp.start();
     }
